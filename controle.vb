bf5270b9e6a9f18ec66ed4bcca217983cbb6c6e8
naomy19 <camillanaomy@gmail.com>
2022-07-25 16:13:47 -0300

    remocao de arquivos

--- controle.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class controle
-    Private Sub controle_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub ctrle_search_icon_Click(sender As Object, e As EventArgs) Handles ctrle_search_icon.Click
-
-    End Sub
-End Class
--- promocao.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class promocao
-
-End Class
